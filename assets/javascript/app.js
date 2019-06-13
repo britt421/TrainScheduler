2626330ffc4d7fdf554f0644f38e535a1d53d02e
Brittany Taylor <hopkins.britt@gmail.com>
2019-06-13 18:20:15 -0500

    working on getting stuff to work in the js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,10 +43,43 @@ db.ref().orderByChild("time").on("child_added", function (snapshot){
     var tf = sv.frequency;      //train frequency
     var tt = sv.start_time;     //train start time
 
+    var diffTime = moment().diff(moment.unix(start_time), "minutes");
+    console.log(diffTime);
+    var timeRemainder = moment().diff(moment.unix(tt), "minutes") % tf;
+    console.log(timeRemainder);
+    var minutes = tf - timeRemainder;
+    console.log(minutes);
+    var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A");
 
+    //Test for crrect times and info
+    console.log("min" + minutes);
+    console.log("nxttrainarr" + nextTrainArrival);
+    console.log("now" + moment().format("hh:mm A"));
+    console.log("next train" + nextTrainArrival);
+    console.log(moment().format("X"));
 
-
-})
+    //Append train info to table on page
+    $("#traindetains").append("<tr>" + 
+        "<td>" + tn + "</td>" +
+        "<td>" + td + "</td>" +
+        "<td>" + tf + "</td>" +
+        "<td>" + nextTrainArrival + "</td>" +
+        "<td>" + minutes + "</td>" +
+        // "<td><button class='btn btn-default btn-sm'id=
+        // "<td><button class='btn btn-default btn-sm'id=
+        "</tr>");
+}),
+function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+}
+$("body").on("click", "#delete", function(e)){
+    e.preventDefault();
+    var key = $(this).attr("data-key");
+    db.ref().orderByChild('id').equalTo(key).once('va')     //incomplete
+    snapshot.forEach(function(childSnapshot){   
+        database.ref().child(childSnapshot.key).remove();   //guessing here
+    })
+}
 
 
 
